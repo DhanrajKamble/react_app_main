9628fea2c02d606e56d0aa7e10457b6fe8944ba8
Dhanraj_Kamble <dhanrajkamble3518@gmail.com>
2025-01-29 17:45:56 +0530

    completed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 // API - Application Programming Interface - a set of rules that allows one software application to talk to another
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -27,16 +28,20 @@ const App = () => {
 
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
+  // Displaying Trending section
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+
   // Debounce the search term to prevent making too many API requests
   // by waiting for the user to stop typing for 500ms
-  useDebounce(()=> setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
   const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = query 
-      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
         : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
@@ -44,31 +49,47 @@ const App = () => {
       // alert(response);
 
       // If there is any error happens
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error("Failed to fetch movies");
       }
 
       const data = await response.json();
       console.log(data);
 
-      if(data.Response === 'False'){
+      if (data.Response === 'False') {
         setErrorMessage(data.Error || "Failed to fetch movies");
         setMovieList([]);
         return;
       }
       setMovieList(data.results || []);
 
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.log(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
-    }finally{
+    } finally {
       setIsLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.log(`Error fetching trending movies: ${error}`);
     }
   }
 
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -85,14 +106,31 @@ const App = () => {
             setSearchTerm={setSearchTerm}
           />
         </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+
         <section className="all-movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+          <h2>All Movies</h2>
 
           {/* {errorMessage && <p className="text-red-500">{errorMessage}</p>} */}
-          {isLoading ? (<Spinner />) : errorMessage ? (<p className="text-red-500">{errorMessage}</p>): (<ul>
-            {movieList.map((movie)=>(
-              <MovieCard key = {movie.id} movie={movie}/>
-          ))}
+          {isLoading ? (<Spinner />) : errorMessage ? (<p className="text-red-500">{errorMessage}</p>) : (<ul>
+            {movieList.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
           </ul>)}
         </section>
 
--- vite-project/src/components/Search.jsx
+++ vite-project/src/components/Search.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-const Search = ({searchTerm, setSearchTerm}) =>{
+const Search = ({ searchTerm, setSearchTerm }) => {
     // 1. props are read only, can't be change its value directly 
     // 2. state term like searchTerm can be ultered only using setSearchTerm
     return (
         <div className="search">
-        <div>
-            <img src="search.svg" alt="search"/>
-            <input 
-                type="text"
-                placeholder="Search through thousands of movies..."
-                value={searchTerm}
-                onChange={(event)=>setSearchTerm(event.target.value)}
-            />
-        </div>
+            <div>
+                <img src="search.svg" alt="search" />
+                <input
+                    type="text"
+                    placeholder="Search through thousands of movies..."
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+            </div>
 
         </div>
-    
+
     );
 
 }
